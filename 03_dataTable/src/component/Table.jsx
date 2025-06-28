e993b723f0ee5c2eca85e511f737f79aaee1a5d0
vibhuti-jadav <vibhutij109@gmail.com>
2025-06-28 12:51:30 +0530

    changes

--- 03_dataTable/src/component/Table.jsx
+++ 03_dataTable/src/component/Table.jsx
@@ -13,6 +13,8 @@ const Table = () => {
   const datas = useSelector((state)=>state.data.data);
   const {sortBy,sortDirection}=useSelector((state)=>state.data)
 
+   
+    
   const handlesort =(field)=>{
     dispatch(sortUsersByField(field));
   };
@@ -44,7 +46,7 @@ const Table = () => {
         </thead>
         <tbody>
           {data.map((ele)=>(
-               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+               <tr key={ele.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                    {ele.id}
                 </th>
@@ -70,6 +72,8 @@ const Table = () => {
         </tbody>
     </table>
 </div>
+
+ 
 
   )
 }
